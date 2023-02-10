bfc7899d7b69cdff25a118544d81c358986769eb
DatabaseDropper <47637776+DatabaseDropper@users.noreply.github.com>
2023-02-10 13:28:59 +0100

    fixing order by

--- OnlineJudge/Web/Controllers/CodeController.cs
+++ OnlineJudge/Web/Controllers/CodeController.cs
@@ -165,7 +165,7 @@ public class CodeController : Controller
         if (tasks.Success)
         {
             var ordered = isAdmin ?
-                tasks.Value.OrderByDescending(x => x.User.Email).ToList() :
+                tasks.Value.OrderBy(x => x.User.Email).ToList() :
                 tasks.Value.OrderByDescending(x => x.Submitted).ToList();
 
             return View(ordered);
